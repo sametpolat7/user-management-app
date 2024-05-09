bc8907ff9611bc6bb33aeaa2584312bcc8c984da
Samet Polat <samet.polat59@hotmail.com>
2024-05-09 15:33:34 +0300

    init: File structure created, dependencies installed
    React App created. Implemented folderization according to MVVM architecture and removed
    default React test and performance tracking files. Downloaded Material
    UI.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import './assets/styles/App.css';
+import '@fontsource/roboto/300.css'; // Light
+import '@fontsource/roboto/400.css'; // Regular
+import '@fontsource/roboto/700.css'; // Bold
+import { Container, Typography } from '@mui/material';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='App'>
+      <Container>
+        <Typography variant='h1'>Hello World!</Typography>
+      </Container>
     </div>
   );
 }
